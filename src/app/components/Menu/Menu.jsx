257b10b7f0bd22a1ac4bb4ad287dcf1dfc9a34b2
Jashu112 <jash22052.cs@rmkec.ac.in>
2024-01-01 12:42:21 +0530

    imported correct modules

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./menu.module.css";
 import Link from "next/link";
-import MenuPosts from "../menuPosts/menuPosts";
-import MenuCategories from "../menuCategories/menuCategories";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = ()=>
 {
