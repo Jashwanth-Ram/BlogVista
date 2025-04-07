e4faa56f05ab44b87227f80224bb5131df450bb7
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-04-07 17:08:48 +0530

    Update Menu.jsx

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -9,7 +9,7 @@ const Menu = ()=>
     return (
         <div className={styles.container}>
 
-            <h2 className={styles.subtitle}>{"What's hot"}</h2>
+            <h2 className={styles.subtitle}>{"What's"}</h2>
             <h1 className={styles.title}>Most Popular</h1>
             <MenuPosts withImage={false}/>
 
