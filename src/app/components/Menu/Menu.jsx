613e968b951aa8ce5c187b128cb8c1671a0f9d92
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-05-06 13:26:24 +0530

    Update Menu.jsx

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -18,10 +18,10 @@ const Menu = ()=>
             <h1 className={styles.title}>Categories</h1>
             <MenuCategories/>
 
-            <h2 className={styles.subtitle}>Chosen by the editor</h2>
-            <h1 className={styles.title}>Editors Pick</h1>
+           <h2 className={styles.subtitle}>Chosen by the editor</h2>
+            <h1 className={styles.title}>Featured Posts</h1>
             <MenuPosts withImage={true}/>
-
+            
         </div>
     )
 }
