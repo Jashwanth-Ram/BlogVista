5e283ae23064a824f08f70c7ee376b85a52e9b1e
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-05-06 12:58:12 +0530

    Update CardList.jsx

--- src/app/components/cardList/CardList.jsx
+++ src/app/components/cardList/CardList.jsx
@@ -27,7 +27,7 @@ const CardList = async ({ page, cat }) => {
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;  
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Recent Posts</h1>
+      <h1 className={styles.title}>Posts</h1>
       <div className={styles.posts}>
         {posts?.map((item) => (
           <Card item={item} key={item._id} />
