0149d385f537c1e1b343f6345f31a7a022cfd667
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-05-18 21:01:06 +0530

    Update Navbar.jsx

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ()=>
         <div className={styles.links}>
         <ThemeToggle/>
         <Link href="/" className={styles.link}>Homepage</Link>
-        <Link href="/" className={styles.link}>About</Link>
+       
         <AuthLinks/>
       </div>
         
