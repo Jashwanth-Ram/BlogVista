6081370a29784c9fc632011930b6a64c8ea57755
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-05-19 10:03:17 +0530

    Update Pagination.jsx

--- src/app/components/pagination/Pagination.jsx
+++ src/app/components/pagination/Pagination.jsx
@@ -2,27 +2,29 @@
 
 import React from "react";
 import styles from "./pagination.module.css";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Pagination = ({ page, hasPrev, hasNext }) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const cat = searchParams.get("cat");
 
   return (
     <div className={styles.container}>
-       <button
+      <button
         className={styles.button}
         disabled={!hasPrev}
-        onClick={() => router.push(`?page=${page - 1}`)}
+        onClick={() => router.push(`?page=${page - 1}${cat ? `&cat=${cat}` : ""}`)}
       >
         Previous
       </button>
       <button
         disabled={!hasNext}
         className={styles.button}
-        onClick={() => router.push(`?page=${page + 1}`)}
+        onClick={() => router.push(`?page=${page + 1}${cat ? `&cat=${cat}` : ""}`)}
       >
         Next
-  </button>
+      </button>
     </div>
   );
 };
