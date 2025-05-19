da546f30e88f145d27f452e36faa05148833251b
Jashwanth Ram <122851926+Jashwanth-Ram@users.noreply.github.com>
2025-05-19 15:08:24 +0530

    Update Featured.jsx

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>
+    <div id="featured-section" className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey there</b>, Discover stories shared by people.
+        <b>Hey there</b>, Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
@@ -16,7 +16,7 @@ const Featured = () => {
 
 
 
-            <h1 className={styles.postTitle}> BlogVista - Your space to Write, Post and Comment.</h1>
+          <h1 className={styles.postTitle}> BlogVista - Your space to Write, Post and Comment.</h1>
           <p className={styles.postDesc}>
           Blog your way using BlogVista! Share your thoughts, upload images, and join the conversation with comments. A  simple platform for expressing  your thoughts.
           </p>
